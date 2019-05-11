d4adbd9e25672b32d957d45d183fae3de596621e
suwei.air <suwei.air@gmail.com>
2019-05-11 21:04:02 +0800

    添加脚本，下载源码

---
+++ conanfile.py
@@ -0,0 +1,22 @@
+from conans import ConanFile, tools
+
+
+class BoostConan(ConanFile):
+    name = "boost"
+    version = "1.70.0"
+    license = "Boost Software License 1.0"
+    url = "https://github.com/suwei-air/conan-boost"
+    homepage = "https://www.boost.org/"
+    description = "Boost provides free peer-reviewed portable C++ source libraries."
+    settings = "os", "compiler", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = ("shared=False", "fPIC=True")
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def source(self):
+        url = "https://dl.bintray.com/boostorg/release/{}/source/boost_{}.zip".format(
+            self.version, self.version.replace(".", "_"))
+        tools.get(url)
